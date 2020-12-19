a2b2b9095654c98c32121cf5d46ac0710b25d5c3
BooksCup <812809680@qq.com>
2020-12-19 14:56:54 +0800

    新增快递公司枚举

---
+++ src/main/java/com/bc/third/party/server/enums/kuaidi100/ExpressCompany.java
@@ -0,0 +1,42 @@
+package com.bc.third.party.server.enums.kuaidi100;
+
+/**
+ * 快递公司编码
+ *
+ * @author zhou
+ */
+public enum ExpressCompany {
+
+    YUNDA("yunda", "韵达快递"),
+    SHUNFENG("shunfeng", "顺丰速运"),
+    JD("jd", "京东物流"),
+    YUANTONG("yuantong", "圆通速递"),
+    ZHONGTONG("zhongtong", "中通快递"),
+    HUITONGKUAIDI("huitongkuaidi", "百世快递"),
+    SHENTONG("shentong", "申通快递"),
+    ;
+
+    ExpressCompany(String companyCode, String companyName) {
+        this.companyCode = companyCode;
+        this.companyName = companyName;
+    }
+
+    private String companyCode;
+    private String companyName;
+
+    public String getCompanyCode() {
+        return companyCode;
+    }
+
+    public void setCompanyCode(String companyCode) {
+        this.companyCode = companyCode;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
